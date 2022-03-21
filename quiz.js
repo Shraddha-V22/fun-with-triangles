f01887c91c84d4fee604777fed3c2d624a6c3783
Shraddha-V22 <shraddhav1935@gmail.com>
2022-03-21 22:31:58 +0530

    complete quiz logic part

--- quiz.js
+++ quiz.js
@@ -1,5 +1,20 @@
-const quizQuestions = document.querySelector("#quiz-questions");
+const quizForm = document.querySelector("#quiz-form");
 const submitBtn = document.querySelector("#submit-btn");
 const outputMsg = document.querySelector("#output");
 
-console.log(quizQuestions);
+const rightAnswers = ["90°", "right angled"];
+
+function calculateScore() {
+    var score=0;
+    var index=0;
+    const formResults = new FormData(quizForm);
+    for(var values of formResults.values()) {
+        if(values === rightAnswers[index]) {
+            score=score+1;
+        }
+        index=index+1;
+    }  
+    outputMsg.innerText = "Your score is " + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
