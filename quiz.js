4b468cdfa9e3e8bff3294b3e640272500a657fd9
Shraddha-V22 <shraddhav1935@gmail.com>
2022-03-21 16:11:12 +0530

    create .html and .js files

---
+++ quiz.js
@@ -0,0 +1,5 @@
+const quizQuestions = document.querySelector("#quiz-questions");
+const submitBtn = document.querySelector("#submit-btn");
+const outputMsg = document.querySelector("#output");
+
+console.log(quizQuestions);
