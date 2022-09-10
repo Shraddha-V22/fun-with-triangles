b81458983c71ab275efc881fa78909a5ea582bfd
Shraddha-V22 <shraddhav1935@gmail.com>
2022-09-10 23:06:19 +0530

    Update hypo.js

--- hypo.js
+++ hypo.js
@@ -8,14 +8,18 @@ function sumOfAnglesSquare(a, b) {
 }
 
 function calculateHypotenuse() {
-    if(sideInputs[0].value < 0 || sideInputs[1].value < 0) {
-        hypoOutput.innerText = "The sides cannot be NEGATIVE!";
-    }
-    else {
-        var hypotenuseSquare = sumOfAnglesSquare(Number(sideInputs[0].value), Number(sideInputs[1].value));
-        var hypotenuseLength = Math.sqrt(hypotenuseSquare).toFixed(2);
-        
-        hypoOutput.innerText = "The Length of Hypotenuse is " + hypotenuseLength + ".";
+    if(sideInputs[0].value && sideInputs[1].value) {
+        if(sideInputs[0].value < 0 || sideInputs[1].value < 0) {
+            hypoOutput.innerText = "The sides cannot be NEGATIVE!";
+        }
+        else {
+            var hypotenuseSquare = sumOfAnglesSquare(Number(sideInputs[0].value), Number(sideInputs[1].value));
+            var hypotenuseLength = Math.sqrt(hypotenuseSquare).toFixed(2);
+            
+            hypoOutput.innerText = "The Length of Hypotenuse is " + hypotenuseLength + ".";
+        }
+    } else {
+        hypoOutput.innerText = "Give all the values!";
     }
 }
 
